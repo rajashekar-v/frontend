e27227b0c57a5a8eeb2362f9943fe44d379885c0
rajashekar_ctewebapps <rajashekar@rpwebapps.com>
2024-09-27 16:51:40 +0530

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>{{ message }}</h1>
+    <h1>{{ welcomeMessage }}</h1>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      message: '',
+      welcomeMessage: '',
+    };
+  },
+  created() {
+    axios.get('http://localhost:5000/api/hello')
+      .then(response => {
+        this.message = response.data.message;
+      })
+      .catch(error => {
+        console.error(error);
+      });
+      axios.get('http://localhost:5000/api/welcome')
+      .then(response => {
+        this.welcomeMessage = response.data.message;
+      })
+      .catch(error => {
+        console.error(error);
+      });  
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/* Add your styles here */
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      },
+      /* '/ws': { 
+        // If you are using WebSockets
+        target: 'http://localhost:5000',
+        ws: true,
+        changeOrigin: true,
+      }, */
+    },
+  },
 })
